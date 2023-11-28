9de348de6a9a7b25d4c222f9a43db1d37b0d0064
Jason Hodges <jasonlhodges@gmail.com>
2023-11-28 17:10:59 -0500

    update vite config based on Brandon's analog-angular-vercel-example

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,40 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  publicDir: 'src/assets',
-  build: {
-    target: ['es2020'],
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [analog()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test.ts'],
-    include: ['**/*.spec.ts'],
-  },
-  define: {
-    'import.meta.vitest': mode !== 'production',
-  },
-}));
+export default defineConfig(({ mode }) => {
+  return {
+    publicDir: 'src/public',
+    optimizeDeps: {
+      include: ['@angular/common', '@angular/forms'],
+    },
+    build: {
+      target: ['es2020'],
+    },
+    plugins: [
+      analog({
+        nitro: {
+          output: {
+            dir: '../../../.vercel/output', // <- Vercel output
+            publicDir: '../../../.vercel/output/static', // <- Vercel output
+          },
+        },
+      }),
+      splitVendorChunkPlugin(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cache: {
+        dir: `../node_modules/.vitest`,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
